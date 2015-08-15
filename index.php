b6f72fb8e3c38700252a9b1953aacb614306088c
stuajnht <stuajnht@gmail.com>
2015-08-15 11:59:33 -0400

    index.php modified to have the main tag enclosing it

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
-
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-	<h3><?php the_title(); ?></h3>
-	<?php the_content(); ?>
-
-<?php endwhile; else: ?>
-	<p><?php _e('Shucks, I\'ve got nothing. Sorry.'); ?></p>
-<?php endif; ?>
-
+<main class="mdl-layout__content">
+	<div>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<h3><?php the_title(); ?></h3>
+			<?php the_content(); ?>
+		<?php endwhile; else: ?>
+			<p><?php _e('Shucks, I\'ve got nothing. Sorry.'); ?></p>
+		<?php endif; ?>
+	</div>
+</main>
 <?php get_footer(); ?>
