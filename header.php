bdf0d61d22691a836ebabacdd71e2694e8d144bc
stuajnht <stuajnht@gmail.com>
2015-08-15 11:56:52 -0400

    Navigation menu added to header.php

--- header.php
+++ header.php
@@ -16,4 +16,44 @@
 		<?php wp_head(); ?>
 	</head>
 	<body class="mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
-		
+		<!-- Navigation header and drawer -->
+		<div class="mdl-layout mdl-js-layout mdl-layout--overlay-drawer-button">
+			<!-- Header -->
+			<header class="mdl-layout__header">
+				<div class="mdl-layout-icon"></div>
+				<div class="mdl-layout__header-row">
+					<!-- Blog title -->
+					<span class="mdl-layout-title"><?php bloginfo('name'); ?></span>
+					<!-- Add spacer, to align navigation to the right -->
+					<div class="mdl-layout-spacer"></div>
+					<!-- Navigation links -->
+					<nav class="mdl-navigation">
+						<!-- WP generated links -->
+						<?php
+							$pages = get_pages(array('parent' => 0, 'hierarchical' => 0));
+							foreach ($pages as $page) {
+								echo '<a class="mdl-navigation__link" href="'.get_page_link( $page->ID ).'">'.$page->post_title.'</a>';
+							}
+						?>
+						<!-- End WP generated links -->
+					</nav>
+				</div>
+			</header>
+			<!-- End header -->
+			<!-- Drawer -->
+			<div class="mdl-layout__drawer">
+				<span class="mdl-layout-title"><?php bloginfo('name'); ?></span>
+				<!-- Navigation links -->
+				<nav class="mdl-navigation">
+					<!-- WP generated links -->
+					<?php
+						$pages = get_pages(array('parent' => 0, 'hierarchical' => 0));
+						foreach ($pages as $page) {
+							echo '<a class="mdl-navigation__link" href="'.get_page_link( $page->ID ).'">'.$page->post_title.'</a>';
+						}
+					?>
+					<!-- End WP generated links -->
+				</nav>
+			</div>
+			<!-- End drawer -->
+			<!-- End navigation header and drawer -->
