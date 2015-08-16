ce948906876322409ad5d39f271bccd6997a74d4
stuajnht <stuajnht@gmail.com>
2015-08-16 17:06:30 -0400

    Tidied up class names and slightly modified some CSS values

--- single.php
+++ single.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
-<div class="demo-ribbon"></div>
-<main class="demo-main mdl-layout__content">
-	<div class="demo-container mdl-grid">
+<div class="single-banner-image"></div>
+<main class="single-main mdl-layout__content">
+	<div class="single-container mdl-grid">
 		<div class="mdl-cell mdl-cell--1-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
-		<div class="demo-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col wow fadeInUp">
+		<div class="single-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--10-col wow fadeInUp">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<h3><?php the_title(); ?></h3>
-				<div class="demo-crumbs mdl-color-text--grey-500">
+				<div class="single-date mdl-color-text--grey-500">
 					<?php the_time('l, F jS, Y'); ?>
 				</div>
 				<?php the_content(); ?>
