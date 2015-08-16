2b5ae3f2ac44999e7efc5ea14196be19eee4ecb7
stuajnht <stuajnht@gmail.com>
2015-08-16 17:44:26 -0400

    Set the post banner image to be displayed on the single post template

--- single.php
+++ single.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
-<div class="single-banner-image"></div>
+<div class="single-banner-image" id="banner-image"></div>
 <main class="single-main mdl-layout__content">
 	<div class="single-container mdl-grid">
 		<div class="mdl-cell mdl-cell--1-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
 		<div class="single-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--10-col wow fadeInUp">
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+				$bannerImage = get_post_meta(get_the_ID(), 'postBannerImage', true); ?>
 				<h3><?php the_title(); ?></h3>
 				<div class="single-date mdl-color-text--grey-500">
 					<?php the_time('l, F jS, Y'); ?>
@@ -16,4 +17,7 @@
 		</div>
 	</div>
 </main>
+<script>
+	document.getElementById("banner-image").style.backgroundImage = "url(<?php echo $bannerImage; ?>)";
+</script>
 <?php get_footer(); ?>
