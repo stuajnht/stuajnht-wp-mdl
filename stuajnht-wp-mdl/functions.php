eac8eb925c342b1ba3f76226b467589e8f3390f9
stuajnht <stuajnht@users.noreply.github.com>
2016-09-09 17:54:56 -0400

    Added menu support to the theme

--- stuajnht-wp-mdl/functions.php
+++ stuajnht-wp-mdl/functions.php
@@ -6,3 +6,21 @@ function stuajnht_wp_mdl_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'stuajnht_wp_mdl_scripts' );
+
+/**
+ * Registering menu locations for the theme
+ *
+ * The available menus for this theme are:
+ *  - Footer Main Menu: Links to pages on this site
+ *  - Footer Social Menu: Links to external sites you have accounts on
+ */
+add_action( 'init', 'stuajnht_wp_mdl_menus' );
+
+function stuajnht_wp_mdl_menus() {
+	register_nav_menus(
+		array(
+			'footer-main-menu' => __( 'Footer Main Menu' ),
+			'footer-social-menu' => __( 'Footer Social Menu' ),
+		)
+	);
+}
