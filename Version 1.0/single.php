f2f24a3a98ccebbecf0f527f5abc2ca73b9a4e07
stuajnht <stuajnht@users.noreply.github.com>
2016-09-09 16:07:53 -0400

    Undid accidental deletion of all files

---
+++ Version 1.0/single.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+<div class="single-banner-image-container">
+	<div class="single-banner-image"></div>
+	<div class="single-banner-image" id="banner-image"></div>
+</div>
+<main class="single-main mdl-layout__content">
+	<div class="single-container mdl-grid">
+		<div class="mdl-cell mdl-cell--1-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
+		<div class="single-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--10-col wow fadeInUp">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+				$bannerImage = get_post_meta(get_the_ID(), 'postBannerImage', true); ?>
+				<h3><?php the_title(); ?></h3>
+				<div class="single-date mdl-color-text--grey-500">
+					<?php the_time('l, F jS, Y'); ?>
+				</div>
+				<?php the_content(); ?>
+			<?php endwhile; else: ?>
+				<p><?php _e('Shucks, I\'ve got nothing. Sorry.'); ?></p>
+			<?php endif; ?>
+		</div>
+	</div>
+</main>
+<script>
+	jQuery(document).ready(function() {
+		var img = new Image();
+		img.onload = function(){
+			jQuery('#banner-image').css({'background-image': 'url(<?php echo $bannerImage; ?>)'});
+			jQuery('#banner-image').fadeIn(1000);
+		}
+		img.src = '<?php echo $bannerImage; ?>';
+	});
+</script>
+<?php get_footer(); ?>
