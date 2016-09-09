20ec7cf2b52624bc103f33286f6ebedd0dd3e75a
stuajnht <stuajnht@users.noreply.github.com>
2016-09-09 18:06:51 -0400

    Initial attempt to add the footer menu to the page

--- stuajnht-wp-mdl/footer.php
+++ stuajnht-wp-mdl/footer.php
@@ -1,4 +1,5 @@
-     <footer class="mdl-mini-footer">
+  <?php if ( has_nav_menu( 'footer-main-menu' ) ) { // Check for footer main menu ?>
+    <footer class="mdl-mini-footer">
       <div class="mdl-mini-footer__left-section">
         <div class="mdl-logo">
           More Information
@@ -7,14 +8,18 @@
           <li><a href="#">Help</a></li>
           <li><a href="#">Privacy and Terms</a></li>
           <li><a href="#">User Agreement</a></li>
+          <?php wp_nav_menu( array( 'theme_location' => 'footer-main-menu' ) ); ?>
         </ul>
       </div>
+      <?php if ( has_nav_menu( 'footer-social-menu' ) ) { // Check for footer social menu ?>
       <div class="mdl-mini-footer__right-section">
         <button class="mdl-mini-footer__social-btn"></button>
         <button class="mdl-mini-footer__social-btn"></button>
         <button class="mdl-mini-footer__social-btn"></button>
       </div>
+      <?php } // End of check for footer social menu ?>
     </footer>
+  <?php } // End of check for footer main menu ?>
 
     <?php wp_footer(); ?>
 	</body>
