fa57e0ca17b5eefe2fa6b3929ecdaadc202d36ec
stuajnht <stuajnht@users.noreply.github.com>
2016-09-09 19:20:26 -0400

    Initial steps to create custom menu HTML for the footer social icons

--- stuajnht-wp-mdl/footer.php
+++ stuajnht-wp-mdl/footer.php
@@ -12,11 +12,17 @@
                                 ) ); ?>
       </div>
       <?php if ( has_nav_menu( 'footer-social-menu' ) ) { // Check for footer social menu ?>
-      <div class="mdl-mini-footer__right-section">
+      <!--<div class="mdl-mini-footer__right-section">
         <button class="mdl-mini-footer__social-btn"></button>
         <button class="mdl-mini-footer__social-btn"></button>
         <button class="mdl-mini-footer__social-btn"></button>
-      </div>
+      </div>-->
+      <?php wp_nav_menu( array(
+                                  'theme_location' => 'footer-social-menu',
+                                  'container_class' => 'mdl-mini-footer__right-section',
+                                  'fallback_cb' => '',
+                                  'walker' => new stuajnht_wp_mdl_walker_nav_footer_social_Menu
+                                ) ); ?>
       <?php } // End of check for footer social menu ?>
     </footer>
   <?php } // End of check for footer main menu ?>
