c05975d2ad2fa680d605b6f66121986eb34bc8d7
stuajnht <stuajnht@users.noreply.github.com>
2016-09-10 15:27:03 -0400

    Added example cards to home.php to see how the layout works

--- stuajnht-wp-mdl/home.php
+++ stuajnht-wp-mdl/home.php
@@ -1,3 +1,42 @@
 <?php get_header(); ?>
 
+<div class="mdl-grid">
+  <!-- Cell -->
+    <div class="mdl-cell mdl-cell--4-col mdl-card mdl-shadow--4dp">
+      <div class="mdl-card__media"><img src="skytower.jpg" width="173" height="157" border="0" alt="" style="padding:10px;">
+      </div>
+      <div class="mdl-card__supporting-text">
+        Auckland Sky Tower, taken March 24th, 2014
+      </div>
+      <div class="mdl-card__supporting-text">
+        The Sky Tower is an observation and telecommunications tower located in Auckland, New Zealand. It is 328 metres (1,076 ft) tall, making it the tallest man-made structure in the Southern Hemisphere.
+      </div>
+    </div>
+  <!-- End Cell -->
+  <!-- Cell -->
+  <div class="mdl-cell mdl-cell--4-col mdl-card mdl-shadow--4dp">
+      <div class="mdl-card__media"><img src="skytower.jpg" width="173" height="157" border="0" alt="" style="padding:10px;">
+      </div>
+      <div class="mdl-card__supporting-text">
+        Auckland Sky Tower, taken March 24th, 2014
+      </div>
+      <div class="mdl-card__supporting-text">
+        The Sky Tower is an observation and telecommunications tower located in Auckland, New Zealand. It is 328 metres (1,076 ft) tall, making it the tallest man-made structure in the Southern Hemisphere.
+      </div>
+    </div>
+  <!-- End Cell -->
+  <!-- Cell -->
+  <div class="mdl-cell mdl-cell--4-col mdl-card mdl-shadow--4dp">
+      <div class="mdl-card__media"><img src="skytower.jpg" width="173" height="157" border="0" alt="" style="padding:10px;">
+      </div>
+      <div class="mdl-card__supporting-text">
+        Auckland Sky Tower, taken March 24th, 2014
+      </div>
+      <div class="mdl-card__supporting-text">
+        The Sky Tower is an observation and telecommunications tower located in Auckland, New Zealand. It is 328 metres (1,076 ft) tall, making it the tallest man-made structure in the Southern Hemisphere.
+      </div>
+    </div>
+  <!-- End Cell -->
+</div>
+
 <?php get_footer(); ?>
