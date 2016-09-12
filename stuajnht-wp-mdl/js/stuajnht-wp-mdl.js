10de326ce0ad2aed2b1e9f0b069bed9779706843
stuajnht <stuajnht@users.noreply.github.com>
2016-09-12 19:50:07 -0400

    Second attempt to prevent the card text fading when the post image is loaded

--- stuajnht-wp-mdl/js/stuajnht-wp-mdl.js
+++ stuajnht-wp-mdl/js/stuajnht-wp-mdl.js
@@ -3,10 +3,17 @@
 
   $(document).ready(function() {
     // Setting up all background images to fade in upon load
-    $('.mdl-card__title').waitForImages(function() {
-      // All descendant images have loaded, now slide up.
-      console.log("hi");
-      $(this).fadeOut(10000);
+    $('.mdl-card__title').waitForImages({
+      each: function() {
+        // All descendant images have loaded, now slide up.
+        console.log("hi");
+        $(this).fadeOut(10000);
+      },
+      waitForAll: true
     });
+    
+    // Preventing parent opacity of the card post images fading
+    // any child classes: http://www.impressivewebs.com/fixing-parent-child-opacity/
+    thatsNotYoChild('mdl-card__title');
   });
 }(jQuery));
