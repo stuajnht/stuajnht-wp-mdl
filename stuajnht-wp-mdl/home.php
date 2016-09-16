16ce1616f34cf6edab931adb43e2eccdc5dd3c3d
stuajnht <stuajnht@users.noreply.github.com>
2016-09-16 16:29:43 -0400

    Pulled the dominant colour from the feature image, which has been created with the 'Dominant Colors Lazy Loading' plugin, and checked to see if it is active

--- stuajnht-wp-mdl/home.php
+++ stuajnht-wp-mdl/home.php
@@ -69,7 +69,13 @@ $cellColumnWidth = 0;
               // of the post image, so there is something to show while
               // the image itself loads
               if (has_post_thumbnail()) {
-                
+                // Before we attempt to get a dominant colour, make sure that
+                // the Dominant_Colors_Lazy_Loading plugin is active
+                if (class_exists('Dominant_Colors_Lazy_Loading')) {
+                  echo " style=\"background-color: #"
+                  . get_post_meta( get_post_thumbnail_id(), 'dominant_color', true )
+                  . ';"';
+                }
               } else {
                 // There isn't a feature image for this post, so get the dominant
                 // colour of the "placeholder" image. The image chosen is based on the
